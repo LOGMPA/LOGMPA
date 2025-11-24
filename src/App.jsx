cdc262f3789d21fa6ee312c5c90eb11633d6c7e3
LOGMPA <mpamarcelamarques@gmail.com>
2025-11-24 13:29:55 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+// src/App.jsx
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import AuthGate from "./AuthGate";
+
 import PainelLogistica from "./pages/PainelLogistica";
 import Calendario from "./pages/Calendario";
 import SolicitacoesTransporte from "./pages/SolicitacoesTransporte";
@@ -9,18 +11,31 @@ import TransportesConcluidos from "./pages/TransportesConcluidos";
 import Demonstracoes from "./pages/Demonstracoes";
 import DashboardCustos from "./pages/DashboardCustos";
 
+import CustosGuard from "./components/auth/CustosGuard";
+
 export default function App() {
   return (
     <AuthGate>
       <Layout>
         <Routes>
           <Route path="/" element={<Navigate to="/painel" replace />} />
+
           <Route path="/painel" element={<PainelLogistica />} />
           <Route path="/calendario" element={<Calendario />} />
           <Route path="/solicitacoes" element={<SolicitacoesTransporte />} />
           <Route path="/concluidos" element={<TransportesConcluidos />} />
           <Route path="/demos" element={<Demonstracoes />} />
-          <Route path="/custos" element={<DashboardCustos />} />
+
+          {/* /custos protegido pela senha MCM */}
+          <Route
+            path="/custos"
+            element={
+              <CustosGuard>
+                <DashboardCustos />
+              </CustosGuard>
+            }
+          />
+
           <Route path="*" element={<Navigate to="/painel" replace />} />
         </Routes>
       </Layout>
